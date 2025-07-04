462256a9e4de1cedf6b559d9077215c78beb29df
Angel Munoz <angel.munoz.sanchez@avanade.com>
2025-07-04 09:42:07 +0200

    fix testing

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-20-mfe');
+    expect(compiled.querySelector('h1')?.textContent).toContain('angular-20-mfe');
   });
 });
